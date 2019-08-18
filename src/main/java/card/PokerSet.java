33867ce0c27c9f88f471e8b6841df8fab27d2efc
unknown <11059964896@qq.com>
2019-08-18 23:52:18 +0800

    refactor: refactor compareTo method and extract compareWithThree method

--- src/main/java/card/PokerSet.java
+++ src/main/java/card/PokerSet.java
@@ -90,17 +90,17 @@ public class PokerSet implements Comparable {
             if(this.getBiggest() == ((PokerSet) o).getBiggest()) return 0;
             return this.getBiggest() > ((PokerSet) o).getBiggest() ? 1 : -1;
         }else if(this.isStraight() == NOT_STRAIGHT && ((PokerSet) o).isStraight() == NOT_STRAIGHT) {
-            if(this.getMaxSameCardNum() == ((PokerSet) o).getMaxSameCardNum()) {
-                if(this.pairNum() == ((PokerSet) o).pairNum()) return compareWithPair((PokerSet) o);
-                return this.pairNum() > ((PokerSet) o).pairNum() ? 1 : -1;
-            }
-            return this.getMaxSameCardNum() > ((PokerSet) o).getMaxSameCardNum() ? 1 : -1;
+            return compareWithThree((PokerSet) o);
         }
-        if(this.isStraight() > ((PokerSet) o).isStraight()){
-            return 1;
-        }else {
-            return -1;
+        return this.isStraight() > ((PokerSet) o).isStraight() ? 1 : -1;
+    }
+
+    private int compareWithThree(PokerSet o) {
+        if(this.getMaxSameCardNum() == o.getMaxSameCardNum()) {
+            if(this.pairNum() == o.pairNum()) return compareWithPair(o);
+            return this.pairNum() > o.pairNum() ? 1 : -1;
         }
+        return this.getMaxSameCardNum() > o.getMaxSameCardNum() ? 1 : -1;
     }
 
     private int compareWithPair(PokerSet o) {
